2c6418dd40a496bb5413cb1ea2808ad290d30c86
Emanuel Gomez <emagozz@gmail.com>
2021-10-18 21:21:41 -0300

    first commit

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['The Office']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+
+            <AddCategory setCategories={ setCategories } />
+
+            <hr/>
+
+            <ol>
+                {
+                    categories.map( category => (
+                        <GifGrid 
+                            key={category}
+                            category= { category } 
+                        />
+                    ))
+                }
+            </ol>   
+        </>
+    );
+};
